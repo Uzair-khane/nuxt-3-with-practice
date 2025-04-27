64bdc3f396451ecd09b0860e98904ae34e95a560
Uzair-Khane <uzairkhanar123@gmail.com>
2025-04-27 09:00:35 +0100

    add chapter 3 and also working on pages

---
+++ chapter-3-components/pages/about.vue
@@ -0,0 +1,9 @@
+
+
+<template>
+   <Header />
+  <div>
+  <h1>this is about page</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, atque! Suscipit pariatur enim possimus magnam, asperiores fugit earum amet voluptas a dolor provident nemo eos quaerat. Sequi pariatur vitae nostrum atque doloribus magnam qui.</p>
+  </div>
+</template>
