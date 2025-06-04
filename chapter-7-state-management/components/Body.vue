b52346f4ba05e86677b926fead538f036fa59519
Uzair-Khane <uzairkhanar123@gmail.com>
2025-06-04 01:12:07 +0100

    add chapter 7 state management

---
+++ chapter-7-state-management/components/Body.vue
@@ -0,0 +1,9 @@
+<script setup>
+const reward = useReward();
+</script>
+<template>
+  <div>
+    <h1>This is component</h1>
+    <h3>{{ reward }}</h3>
+  </div>
+</template>
---
+++ chapter-7-state-management/pages/About.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+<template>
+  <div>
+    <h1>This is about page</h1>
+      <Body/>
+  </div>
+</template>
