f1faba90c7b554887ea6fc007c539260ccc20cb4
Uzair-Khane <uzairkhanar123@gmail.com>
2025-06-15 23:55:55 +0100

    add chapter 8

---
+++ chapter-8-Middleware/nuxt-app/app.vue
@@ -0,0 +1,15 @@
+<script setup>
+definePageMeta({
+  middleware : function(context){
+    console.log("this is middleware ");
+    console.log(context);
+    
+    
+  },
+});
+</script>
+<template>
+  <div>
+   
+  </div>
+</template>
