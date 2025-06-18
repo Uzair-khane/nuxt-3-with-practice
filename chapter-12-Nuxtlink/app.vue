36b2f81ebb3f95cf7eaa881caa60fb0190c62e77
Uzair-Khane <uzairkhanar123@gmail.com>
2025-06-19 00:48:34 +0100

    add chapter 13 nuxtlink

---
+++ chapter-12-Nuxtlink/app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <!-- <nuxt-link to="/U.psd " external>downlode file</nuxt-link> -->
+    <br />
+    <!-- external link  -->
+    <nuxt-link to="https://nuxt.com/docs/api/components/nuxt-link"
+      >Nuxt 3</nuxt-link
+    >
+    <br />
+    <!-- open in new tab  -->
+    <nuxt-link
+      to="https://chatgpt.com/c/6853b0c4-22f0-800d-b4be-5086c29163a3"
+      target="_blank"
+      >Open chatgpt in new page</nuxt-link
+    > <br>
+    <!-- prefetch -->
+     <nuxt-link to="https://www.youtube.com/" prefetch>Go to Youtube</nuxt-link>
+  </div>
+</template>
