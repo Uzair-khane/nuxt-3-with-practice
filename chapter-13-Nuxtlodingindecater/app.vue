136241a28275ea2ccab1ddc3918ddfae610d4c82
Uzair-Khane <uzairkhanar123@gmail.com>
2025-06-19 01:51:38 +0100

    add chapter 13 Nuxtlodingindecater

---
+++ chapter-13-Nuxtlodingindecater/app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <NuxtLoadingIndicator 
+      color="#3498db"
+      error-color="red"
+      height="5"
+      duration="1500"
+      throttle="100"
+    />
+    <NuxtPage />
+  </div>
+</template>
